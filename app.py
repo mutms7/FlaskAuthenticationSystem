ee7e4b43e4710abb5f17c4430666bd8bea89bb2a
William Chenyin <welcomehomemutms7@gmail.com>
2026-01-10 17:56:42 -0500

    m

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from database import config
 
 
 # instance from Flask
 app=Flask(__name__)
 
+# config SQLAlchemy 
+app.config('SQLALCHEMY_DATABASE_URI') = config.DATABASE_CONNECTION_URI
 
-
+#mysql://username:password@server/db
 
 @app.route('/')
 def home():
---
+++ database/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATEBASE_NAME=  os.getenv("DATABASE_NAME")
+DATABASE_USER =  os.getenv("DATABASE_USER")
+DATABASE_PASSWORD =  os.getenv("DATABASE_PASSWORDS")
+
+DATABASE_CONNECTION_URI='mysql://{DATEBASE_NAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATEBASE_NAME}'
